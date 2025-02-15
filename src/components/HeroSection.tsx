2bd33fde9f8c161d9bd51c3ac77cc007a6462a31
Alex Glancevs <alex2531458@gmail.com>
2025-02-15 23:35:12 +0200

    feat: create hero section component

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+    return (
+        <section className="hero">
+            <h1 className="hero__header">
+                We’re a small indie game team bringing fresh ideas to life
+            </h1>
+            <p className="hero__subhead">
+                Check out our games, playtest prototypes, and follow our journey 🚀
+            </p>
+        </section>
+    )
+}
