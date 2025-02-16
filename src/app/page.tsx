945ec51d9c2f878856e3fed99f63772d7551e919
Alex Glancevs <alex2531458@gmail.com>
2025-02-16 23:16:13 +0200

    feat: add team section (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import GamesSection from "@/components/GamesSection";
 import HeroSection from "@/components/HeroSection";
+import TeamSection from "@/components/TeamSection";
 
 export default function Home() {
     return (
         <main>
             <HeroSection />
             <GamesSection />
+            <TeamSection />
         </main>
     )
 }
